75d5a3cb99ed4195ca6e646813d82be212ba1535
arfancitraw <wibowoarfancitra@gmail.com>
2019-01-17 13:52:01 +0700

    fist commit

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,16 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
+
     return $request->user();
 });
+
+
+
+Route::apiResources(['user' => 'API\UserController']);
+Route::get('profile', 'API\UserController@profile');
+Route::get('findUser', 'API\UserController@search');
+Route::put('profile', 'API\UserController@updateProfile');
